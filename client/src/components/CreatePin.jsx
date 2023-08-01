eca717be3aff8b7dedc4c85c35b4ef188dc50616
FocusSTmatt <113565590+FocusSTmatt@users.noreply.github.com>
2023-07-31 19:42:09 -0700

    .

--- client/src/components/CreatePin.jsx
+++ client/src/components/CreatePin.jsx
@@ -87,8 +87,8 @@ const CreatePin = ({ user }) => {
       {fields && (
         <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in ">Please add all fields.</p>
       )}
-      <div className=" flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5  w-full">
-        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+      <div className=" flex lg:flex-row flex-col justify-center items-center dark:bg-black bg-white lg:p-5 p-3 lg:w-4/5  w-full">
+        <div className="bg-secondaryColor dark:bg-dark p-3 flex flex-0.7 w-full">
           <div className=" flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
             {loading && (
               <Spinner />
@@ -141,7 +141,7 @@ const CreatePin = ({ user }) => {
 
         <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
         {user && (
-            <div className="flex justify-center gap-2 mt-2 mb-2 items-center bg-white rounded-lg ">
+            <div className="flex justify-center gap-2 mt-2 mb-2 items-center dark:bg-black bg-white rounded-lg ">
               <img
                 src={user.image}
                 className="w-10 h-10 rounded-full"
@@ -155,7 +155,8 @@ const CreatePin = ({ user }) => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Add your title"
-            className="outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2"
+            className="outline-none text-2xl sm:text-3xl dark:bg-dark font-bold border-b-2 border-gray-200 p-2"
+            required
           />
           
           <input
@@ -163,28 +164,30 @@ const CreatePin = ({ user }) => {
             value={about}
             onChange={(e) => setAbout(e.target.value)}
             placeholder="Tell everyone what your Pin is about"
-            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+            className="outline-none text-base sm:text-lg border-b-2 dark:bg-dark border-gray-200 p-2"
+            required
           />
           <input
             type="url"
             vlaue={destination}
             onChange={(e) => setDestination(e.target.value)}
             placeholder="Add a destination link"
-            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+            className="outline-none text-base sm:text-lg border-b-2 dark:bg-dark border-gray-200 p-2"
+            required
           />
 
           <div className="flex flex-col">
             <div>
-              <p className="mb-2 font-semibold text:lg sm:text-xl">Choose Pin Category</p>
+              <p className="mb-2 dark:bg-black dark:text-white font-semibold text:lg sm:text-xl">Choose Pin Category</p>
               <select
                 onChange={(e) => {
                   setCategory(e.target.value);
                 }}
-                className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+                className="outline-none w-4/5 text-base border-b-2 dark:bg-dark dark:border-black border-gray-200 p-2 rounded-md cursor-pointer"
               >
-                <option value="others" className="sm:text-bg bg-white">Select Category</option>
+                <option value="others" className="sm:text-bg dark:bg-black dark:text-black bg-white">Select Category</option>
                 {categories.map((item, index) => (
-                  <option key={index} className="text-base border-0 outline-none capitalize bg-white text-black " value={item.name}>
+                  <option key={index} className="text-base border-0 outline-none capitalize dark:bg-black bg-white dark:text-white text-black " value={item.name}>
                     {item.name}
                   </option>
                 ))}
